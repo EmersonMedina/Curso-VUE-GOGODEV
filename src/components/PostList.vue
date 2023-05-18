d8de3ae25308d93cb81ee24e32ddc2128017f19a
EmersonMedina <emermedina877@gmail.com>
2023-05-17 22:09:23 -0600

    Ejercicios de la clase

---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Listado de posts</h1>
+
+  <ul class="post-list">
+    <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import PostService from "@/services/PostService";
+
+const service = new PostService();
+let posts = service.getPosts();
+
+onMounted(async () => {
+  await service.fetchAll();
+});
+</script>
+
+<style scoped>
+.post-list {
+  list-style-type: none; /* Oculta la viñeta predeterminada */
+  margin-left: 20px; /* Margen izquierdo para la sangría */
+}
+</style>
---
+++ src/services/PostService.ts
@@ -0,0 +1,26 @@
+import { ref, Ref } from "vue";
+import IPost from "@/interfaces/IPost";
+
+class PostService {
+  private posts: Ref<IPost[]>;
+
+  constructor() {
+    this.posts = ref<IPost[]>([]);
+  }
+  getPosts(): Ref<IPost[]> {
+    return this.posts;
+  }
+
+  async fetchAll(): Promise<void> {
+    try {
+      const url = "https://jsonplaceholder.typicode.com/posts";
+      const response = await fetch(url);
+      const json = await response.json();
+      this.posts.value = json;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export default PostService;
