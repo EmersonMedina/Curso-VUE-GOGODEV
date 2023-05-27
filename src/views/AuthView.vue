13127ffc98806d365c8bd262d706740386986368
EmersonMedina <emermedina877@gmail.com>
2023-05-27 12:50:43 -0600

    Formulario base para estructuras de autenticación

---
+++ src/views/AuthView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="email"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          v-model="password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary mt-2">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Ref, ref } from "vue";
+
+let email: Ref<string> = ref("");
+let password: Ref<string> = ref("");
+</script>
+
+<style scoped></style>
