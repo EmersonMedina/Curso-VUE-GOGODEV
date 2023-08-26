ff6b8b619c4d4520b3e511de5309a42ca48ac26e
EmersonMedina <emermedina877@gmail.com>
2023-08-26 12:43:24 -0600

    PINIA

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <CounterComponent />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="ts" setup>
+import CounterComponent from './components/CounterComponent.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
 </script>
 
 <style>
---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Counter: {{ store.count }}</h1>
+  <h2>Counter x 2 = {{ store.times2 }}</h2>
+  <button @click="store.increment(1)">increment</button>
+</template>
+
+<script lang="ts" setup>
+  import { useCounterStore } from '../store/counter'
+  const store = useCounterStore()
+</script>
+
