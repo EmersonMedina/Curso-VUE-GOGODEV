49cb7b4ba4f078c270a58a2bb89cc5a31e03fbdf
EmersonMedina <emermedina877@gmail.com>
2023-08-25 19:11:50 -0600

    Vuex

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <h2>Counter</h2>
+  <h3>{{ counter }} x2 = {{ times2 }}</h3>
+  <button @click="increment">Increment</button>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, computed } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const counter = computed(() => store.state.counter);
+const times2 = computed(() => store.getters.times2);
+const increment = () => store.commit('setCounter', counter.value + 1);
+
+// Define 'msg' as a prop
+const props = defineProps({
+msg: String
+});
+</script>
+
