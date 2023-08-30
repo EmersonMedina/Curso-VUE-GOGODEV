6223f16dec44abf5f8d9a02c7a239c8edf544f8b
EmersonMedina <emermedina877@gmail.com>
2023-08-30 14:24:23 -0600

    Protección de rutas

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="text" v-model="note">
+  <button @click="sendToServer">Enviar nota al servidor</button>
+
+  <RouterView></RouterView>
+</template>
+
+<script lang="ts" setup>
+  import {ref} from 'vue'
+
+  let note = ref('')
+
+  const sendToServer = async () => {
+    const rawResponse = await fetch('http://aaaaaaaaaaa', {
+      method: 'POST', 
+      headers: {
+        'Content-Type' : 'application/json', 
+        'Accept': 'application/json',
+        'Authorization': 'Bearer abcdejwt'
+      }, 
+      body: JSON.stringify({ note: note.value })
+    })
+    
+    const response = await rawResponse.json()
+  }
+</script> 
+
