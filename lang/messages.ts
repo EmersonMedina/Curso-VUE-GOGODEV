2dc913996be74ef9ecb05f2be2983dc7b7f76b8b
EmersonMedina <emermedina877@gmail.com>
2023-10-27 21:55:28 -0600

    Trabajo del episodio

---
+++ lang/messages.ts
@@ -0,0 +1,18 @@
+export const messages = { 
+    en: { 
+        message: {
+            hello: 'Hello {name} {surname}', 
+            content: 'We are using i18n package.',
+            products: 'No products | One product | { count } products', 
+            comments: 'No comments | One comment | comments'
+        }
+    }, 
+    es: { 
+        message: {
+            hello: 'Hola {name} {surname}', 
+            content: 'Estamos usando el paquete i18n.', 
+            products: 'No hay productos | Un producto | { count } productos', 
+            comments: 'Sin comentarios  | Un comentario | comentarios'
+        }
+    }
+}
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div>
+    <select v-model="$i18n.locale">
+      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
+          {{ locale }}
+      </option>
+    </select>
+  </div>
+
+  <h1>{{ $t("message.hello", {name: username, surname: surname}) }}</h1>
+  <p>{{ $t("message.content") }}</p>
+  <p> {{ $t("message.products", { count: 0 }) }}</p>
+  <p> {{ $t("message.comments", { count: 5 }) }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const username = ref('Emerson')
+const surname = ref('Medina')
+</script>
+
+<style scoped>
+
+</style>
