4f35b7ff93fbc053c4406e36ffd1cf462524b504
EmersonMedina <emermedina877@gmail.com>
2024-03-05 10:08:42 -0600

    Accediendo a la cámara

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,39 @@
 <template>
-// PARA TABS
- <!-- <ion-app>
-  <ion-router-outlet></ion-router-outlet>
- </ion-app> -->
-
-//PARA MENU
- <!-- <ion-menu content-id="main-content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Menu Example</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content class="ion-padding">
-      I'm the main content
-    </ion-content>
- </ion-menu>
- <ion-page id="main-content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-menu-button/>
-        </ion-buttons>
-        <ion-title>Menu Example</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content class="ion-padding">
-      Tap to open
-    </ion-content>
- </ion-page> -->
-
  <ion-page>
   <ion-header>
     <ion-toolbar>
-      <ion-title>GRID EXAMPLE</ion-title>
+      <ion-title>Capacitor Plugins</ion-title>
     </ion-toolbar>
   </ion-header>
   <ion-content>
-    <ion-row>
-      <ion-col size="12" class="full">
-        Hello World full width
-      </ion-col>
-      <ion-col size="12" sizeSm="6" class="mid">
-        Hello World 50% width
-      </ion-col>
-    </ion-row>
+    <img :src="imageURL"/>
+    <button @click="makePhoto">Take Photo</button>
   </ion-content>
  </ion-page>
 
 </template>
 
 <script setup lang="ts">
-  import { IonApp, IonRouterOutlet, IonPage, IonHeader, IonButtons, IonContent, IonTitle, IonMenuButton } from '@ionic/vue'; 
+  import { IonPage, IonHeader, IonToolbar, IonContent, IonTitle } from '@ionic/vue';
+  import { Camera, CameraResultType } from "@capacitor/camera"
+  import { ref } from "vue";
+
+  const imageURL = ref()
+
+  const makePhoto = async () => {
+    const image = await Camera.getPhoto({
+      quality: 80, 
+      allowEditing: true, 
+      resultType: CameraResultType.Uri
+    }); 
+
+    imageURL.value = image.webPath;
+  }
 </script>
 
 <style scoped>
-  .ion-padding { 
-    padding: 1rem;
-  }
-
-  .full {
-    background-color: aquamarine;
-  }
-
-  .mid {
-    background-color: blanchedalmond;
+  image {
+    width: 80vw;
+    max-height: 60vh;
   }
 </style>
