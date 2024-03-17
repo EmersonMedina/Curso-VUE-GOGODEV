41a6cc1e86391ed7e63420446967808623226097
EmersonMedina <emermedina877@gmail.com>
2024-03-16 22:50:44 -0600

    Autoimport, ssr, spa, hybrid

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: true, 
+  nitro: {
+    prerender: { 
+      routes:['/proyects', '/services', '/about', '/contact', '/' ] 
+    }
+  }, 
+  routeRules: {
+    '/': { prerender: true}, 
+    '/about': { ssr: true }, 
+    '/services': { ssr: false } 
+  }
 })
--- pages/services.vue
+++ pages/services.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Services view</h1>
+  <HelloWorld/>
 </template>
 
 <script lang="ts" setup>
